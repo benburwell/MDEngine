cb52252211fd0ab89581a2e6836c323f896334ec
Ben Burwell <bburwell1@gmail.com>
2012-04-20 23:37:12 -0400

    Version 1.0

---
+++ theme/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $title.' - '.SITE; ?></title>
+		<link rel="stylesheet" type="text/css" href="~style.css" />
+	</head>
+	<body>
+		<header>
+			<h1><a href="<?php echo WEBROOT; ?>"><?php echo SITE; ?></a></h1>
+		</header>
+		<article>
