487b2b851ab4ca433858ca2ed4d7fefd19d17b67
Ben Burwell <bburwell1@gmail.com>
2013-04-16 17:28:41 -0400

    Moved CSS files

--- _mdengine/views/header.php
+++ _mdengine/views/header.php
@@ -4,10 +4,10 @@
 		<title><?php echo $title; ?></title>
 
 		<!-- YUI Reset CSS v3 -->
-		<link rel="stylesheet" type="text/css" href="/_mdengine/views/yui-reset.css" />
+		<link rel="stylesheet" type="text/css" href="/_mdengine/views/css/yui-reset.css" />
 
 		<!-- MDEngine Default Styles -->
-		<link rel="stylesheet" type="text/css" href="/_mdengine/views/style.css" />
+		<link rel="stylesheet" type="text/css" href="/_mdengine/views/css/base.css" />
 
 		<!-- jQuery 1.9.1 -->
 		<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
